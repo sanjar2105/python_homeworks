ed9db79035ac68286ea59741d091a48eb7e1cd3c
sanjar2105 <sanjarsotimboyevv@gmail.com>
2025-05-14 19:28:01 +0500

    Create dars1.py

---
+++ lesson-1/homework/dars1.py
@@ -0,0 +1,227 @@
+#win + r
+
+# dir -- TO SEE WHAT YOU HAVE IN THE CURRENT DIRECTORY
+# CD(CHDIR) -- TO CHANGE DIRECTORY
+
+# CTRL + FORWARD SLASH(/)
+
+
+#Variable Naming
+
+# Faqat harf yoki pastki chiziqcha(underscore) bilan boshlash mumkin
+# Son bilan boshlanishi mumkin emas
+# Faqat harf, son yoki underscore ishlatsa bo'ladi
+# Variable Names are Case Sensitive
+# Python keyword larni variable name ga berish mumkin emas(berilmaydi)
+
+
+# ism = 'John'
+
+# print(ism)
+
+# print(id(ism))
+
+# id() returns the id of a given object
+# Ism  = 'Anna'
+
+# ISM = 'Sarah'
+
+# first_name = 'Josh'
+
+
+# ctrl + z     o'zgartirgan narsalaringizni orqaga qaytaradi
+
+
+# Numbers(Numerics)
+
+# int
+# float
+# complex
+
+# i j
+
+
+# int
+
+# type() returns the data type of a given object
+
+# age = 18
+
+# print(age, id(age), type(age))
+
+
+# age = '18'
+# # print(age,id(age))
+
+# # print(age, id(age), type(age))
+# print(type(age))
+
+# # int() converts values from other data type to int
+# age = int(age)
+
+# print(type(age))
+
+
+# float
+
+# price = 15.50
+
+
+# print(type(price))
+
+# salary = '65.10'
+
+# print(salary)
+
+# print(type(salary))
+
+
+# float() converts the value of a given object to float
+
+
+# price_float = float(salary)
+
+# print(type(price_float))
+
+
+
+## Complex
+
+
+# complex = 1 + 2j
+
+# print(complex)
+
+# print(complex.imag)
+
+# print(complex.real)
+
+
+#complex(a,b) complex(5, 20)
+
+# example = complex(5,20)
+
+# print(example.imag)
+
+
+# world_population = 8_000_000_000
+
+
+# print(world_population-1)
+
+
+## Numberic Operations
+
+
+summa = 5 + 8 + 10
+
+
+
+# print(summa)
+
+subtract = 25 - 12 + 10
+
+# print(subtract)
+
+oybek = 5 + 2
+
+# print(oybek)
+
+
+# devision
+
+
+bolinma = 20 / 2
+
+# print(bolinma)
+
+
+# multiplication
+
+kopaytma = 5 * 9
+
+# print(kopaytma + bolinma)
+
+## to get the integer part of a devision
+
+
+whole_part = 19 // 4
+
+# print(whole_part)
+
+
+# remainder(qoldiq)
+qoldiq = 19 % 4
+
+# print(qoldiq)
+
+percent = 19 / 100 * 4
+
+# print(percent)
+
+
+## // + %
+
+
+#divmod
+
+example = divmod(55,3)
+ 
+
+# print(example)
+
+
+## power
+
+# powerr = power(9,3), a**b
+
+
+powerr = 9**3
+
+# print(powerr)
+
+
+#### Augmented Assignment
+
+number = 15
+
+# number = number + 10
+
+number += 10
+
+# print(number)
+
+
+
+number -= 8
+
+# print(number)
+
+
+number1 = 13
+
+number1 /= 3
+# 13 / 3 = 4.333333333
+# print(number1)
+
+# 4.3333 x 5 = 21.666665
+number1 *= 5
+
+# print(number1)
+
+
+
+number2 = 11
+
+number2 **= 3
+
+print(number2)
+
+
+number = 10
+
+number = 15
+
+print(number)
+
+# ctrl + s
